aa5a7543f789d1069adf90a039aae75ad8b4ac5a
Александр <a.ermakov@maximaster.ru>
2019-05-17 19:29:01 +0300

    Added index page

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$('#year').text(new Date().getFullYear());
+
+$('.carousel').carousel({
+    interval: 6000,
+    pause: 'hover'
+});
+
+$(function () {
+    // Auto play modal video
+    $(".video").click(function () {
+        let theModal = $(this).data("target"),
+            videoSRC = $(this).attr("data-video"),
+            videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=0&showinfo=0&html5=1&autoplay=1";
+        $(theModal + ' iframe').attr('src', videoSRCauto);
+        $(theModal + ' button.close').click(function () {
+            $(theModal + ' iframe').attr('src', videoSRC);
+        });
+    });
+});
+
+$(document).on('click', '[data-toggle="lightbox"]', function(event) {
+    event.preventDefault();
+    $(this).ekkoLightbox();
+});
+
+$('body').scrollspy({
+    target: '#main-nav'
+});
+
+$('#main-nav a').on('click', function(event){
+    if(this.hash !== '') {
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function() {
+            window.location.hash = hash;
+        })
+    }
+})
